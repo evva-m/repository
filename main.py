bef7e1b76c4bd1b84dd24c1f2341da1e41f837b6
Ева <eva56m@yandex.ru>
2024-12-10 14:22:17 +0500

    main.py, UI.py, случайный цвет

--- main.py
+++ main.py
@@ -3,52 +3,15 @@ from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QColor, QPainter
 from PyQt6.QtCore import Qt, QPointF
 from random import randrange
-from PyQt6 import uic
-import io
+from UI import Ui_Form
 
 
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Form</class>
- <widget class="QWidget" name="Form">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>610</width>
-    <height>536</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <widget class="QPushButton" name="btn">
-   <property name="geometry">
-    <rect>
-     <x>270</x>
-     <y>30</y>
-     <width>75</width>
-     <height>23</height>
-    </rect>
-   </property>
-   <property name="text">
-    <string>PushButton</string>
-   </property>
-  </widget>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
-
-class Suprematism(QWidget):
+class Suprematism(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.setupUi(self)
         self.do_paint = False
         self.btn.clicked.connect(self.btn_clicked)
-
 
     def btn_clicked(self):
         self.do_paint = True
@@ -59,7 +22,7 @@ class Suprematism(QWidget):
             qp = QPainter()
             qp.begin(self)
             a = randrange(20, 100)
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randrange(0, 256), randrange(0, 256), randrange(0, 256)))
             qp.drawEllipse(QPointF(200, 200), a, a)
         self.do_paint = False
 
